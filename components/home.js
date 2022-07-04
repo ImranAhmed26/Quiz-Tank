d367c55f387f82610e20bb2b8d44449a2f5c7d85
ImranAhmed26 <17-35401-3@student.aiub.edu>
2022-07-05 02:46:35 +0600

    added the banner image and user name input tag

--- components/home.js
+++ components/home.js
@@ -1,21 +1,41 @@
 import Image from "next/image";
-import mainImage from "../public/assets/images/bi1.png";
+import bannerImage from "../public/assets/images/banner-image.jpg";
+import { useState } from "react";
 
-export default function HomeText() {
+export default function Home() {
+  const [name, setName] = useState("");
+
   return (
-    <div className="py-12">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
-        <div className="text-center">
-          <p className="2xl:mt-16 mb-2 text-5xl leading-10 font-bold tracking-tight text-indigo-900 sm:text-8xl ">
+    <div className="py-28 ">
+      <div className="flex max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 justify-between items-center">
+        <div className="text-center w-1/2">
+          <p className="2xl:mt-16 mb-2 text-5xl leading-10 font-bold tracking-tight text-green-500 drop-shadow-lg sm:text-8xl ">
             Quiz Tank
           </p>
-          <h2 className="text-3xl text-gray-600 font-bold tracking-wide">Develop your GK</h2>
-          <p className="mt-4 max-w-2xl text-xl text-gray-400 lg:mx-auto md:mx-auto">Lorem Ipsum.</p>
+          <h2 className="text-2xl text-gray-600 font-bold tracking-wide">
+            Develop your General Knowledge
+          </h2>
+          <p className="mt-4 max-w-2xl text-xl text-gray-400 lg:mx-auto md:mx-auto">
+            Enter your name to start a quiz
+          </p>
+          <div className="p-10">
+            <input
+              onChange={(event) => {
+                setName(event.target.value || "");
+              }}
+              value={name}
+              className="w-68 h-12 shadow-green-500 p-4 rounded-sm border-2 ring-offset-0 ring-0 outline-0 text-center text-xl text-gray-700 font-semibold"
+              type="text"
+              name="admin_name"
+              id="admin_name"
+              placeholder="Enter Your Name"
+            />
+          </div>
         </div>
 
-        <div className="max-w-4xl justify-self-center flex-initial item-center justify-center lg:mx-auto md:mx-auto ">
+        <div className="w-1/2 max-w-[34rem] item-center justify-center mx-auto ">
           <div className="">
-            <Image className="" src={mainImage} alt="banner image" />
+            <Image className="" src={bannerImage} alt="banner image" />
           </div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
-import Head from 'next/head'
-import Navbar from '../components/navbar'
+import Head from "next/head";
+import Navbar from "../components/navbar";
+import Home from "../components/home";
 
-export default function Home() {
+export default function MainPage() {
   return (
     <div>
       <Head>
         <title>Quiz Up</title>
-        <meta name='description' content='Online Quiz App' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="Online Quiz App" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Navbar />
+      <Home />
     </div>
-  )
+  );
 }
